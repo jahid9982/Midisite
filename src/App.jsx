6b40a53ecaf1f1df985bdecdb2b65c5cbe3e195a
jahid9982 <rayhan9982@gmail.com>
2025-07-10 00:31:56 +0600

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import Footer from "./Pages/Shared/Footer/Footer"
+import Header from "./Pages/Shared/Header/Header"
 
 function App() {
 
 
   return (
-    <div class="text-3xl font-bold underline">
-      <h1>App is runing and git hub jahid branch</h1>
-      <button className="btn btn-dash btn-success">Success</button>
+    <div>
+      <Header></Header>
+      <Footer></Footer>
     </div>
   )
 }
